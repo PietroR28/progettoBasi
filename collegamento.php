bef71d001608754a1e9b7632148be36cd8163c14
PietroR28 <pietro.rizzo@studio.unibo.it>
2025-03-31 18:54:07 +0200

    collegamento
    Signed-off-by: PietroR28 <pietro.rizzo@studio.unibo.it>

---
+++ collegamento.php
@@ -0,0 +1,28 @@
+
+<?php
+
+$host = "127.0.0.1:3306";
+$user = "root";
+$password = "";
+$database = "progettoBasiDiDati_db";
+
+$connessione = new mysqli($host, $user, $password, $database);
+
+if ($connessione->connect_error) {
+    die("Errore di connessione: " . $connessione->connect_error);
+}
+
+
+
+echo "connessione avvenuta con successo: " . $connessione->host_info;
+
+$connessione->close();
+
+?>
+
+
+
+
+
+
+
